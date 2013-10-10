4246a8a7abb0c8d056c21e47e777ae390a434b2a
Tomoya Chiba <tomo.asleep@gmail.com>
2013-10-10 20:25:07 +0900

    update opcode header generator

--- utils/opcode_gen.rb
+++ utils/opcode_gen.rb
@@ -1,7 +1,9 @@
 require "erb"
 require "yaml"
 
-@opcodes = (YAML.load(File.read File.expand_path("../data/opcode.yml", __FILE__)))["r_op"]
+@opcodes = Hash.new
+yaml = (YAML.load(File.read File.expand_path("../data/opcode.yml", __FILE__)))
+yaml.each { |k, v| v.each { |name, num| @opcodes["#{k}_#{name}"] = num }}
 erb = ERB.new(File.read File.expand_path("../templetes/opcode.vhd.erb", __FILE__))
 print erb.result(binding)
 
