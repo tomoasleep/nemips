7855c4b8541cd6859f8cad5ceb9daa88a501f6c0
Tomoya Chiba <tomo.asleep@gmail.com>
2013-10-14 20:40:10 +0900

    Add fsm test and prototype of fsm in VHDL

---
+++ src/fsm.vhd
@@ -0,0 +1,120 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.const_state.all;
+use work.const_opcode.all;
+
+-- DOCTEST DEPENDENCIES: const/const_fsm.vhd, const/opcode.vhd
+-- TEST
+-- /TEST
+
+entity fsm is
+  port(
+        opcode: in std_logic_vector(5 downto 0);
+        funct: in std_logic_vector(5 downto 0);
+        alu_bool_result: in std_logic;
+        reset: in std_logic;
+
+        state: out std_logic_vector(4 downto 0);
+        clk : in std_logic
+      );
+end fsm;
+
+architecture behave of fsm is
+  signal opcode_r: std_logic_vector(5 downto 0) := "000000";
+  signal funct_r:  std_logic_vector(5 downto 0) := "000000";
+  signal current_state: std_logic_vector(4 downto 0);
+begin
+  main: process(clk) begin
+    if rising_edge(clk) then
+      case reset is 
+        when '1' =>
+          current_state <= state_fetch;
+          opcode_r <= "000000";
+          funct_r <= "000000";
+        when others =>
+
+          case current_state is
+            when state_fetch =>
+              current_state <= state_decode;
+
+            when state_decode =>
+              opcode_r <= opcode;
+              funct_r  <= funct;
+
+              case opcode is
+                when i_op_r_group =>
+                  case funct is
+                    when r_fun_jr =>
+                      current_state <= state_jmpr;
+                    when r_fun_jalr =>
+                      current_state <= state_jalr;
+                    when r_fun_mul | r_fun_mulu 
+                    | r_fun_div | r_fun_divu =>
+                      current_state <= state_alu;
+                    when others =>
+                      current_state <= state_alu;
+                  end case;
+                when i_op_beq | i_op_bne | i_op_bltz -- i_op_bgez, i_op_blez
+                | i_op_bgtz =>
+                  current_state <= state_branch;
+                when i_op_lb | i_op_lh | i_op_lw 
+                | i_op_sb | i_op_sh | i_op_sw 
+                | i_op_lwf | i_op_swf =>
+                  current_state <= state_memadr;
+                when j_op_j => 
+                  current_state <= state_jmp;
+                when j_op_jal => 
+                  current_state <= state_jal;
+                when others =>
+                  current_state <= state_alu_imm;
+              end case;
+
+            when state_memadr =>
+              case opcode_r is
+                when i_op_lb | i_op_lh | i_op_lw 
+                | i_op_lwf =>
+                  current_state <= state_mem_read;
+                when i_op_sb | i_op_sh | i_op_sw 
+                | i_op_swf =>
+                  current_state <= state_mem_write;
+                when others =>
+                  current_state <= state_fetch;
+              end case;
+
+            when state_mem_read =>
+              case opcode_r is
+                when i_op_lb | i_op_lh | i_op_lw =>
+                  current_state <= state_mem_wb;
+                when i_op_lwf =>
+                  current_state <= state_mem_wbf;
+                when others =>
+                  current_state <= state_fetch;
+              end case;
+
+            when state_alu =>
+              case funct_r is
+                when r_fun_mul | r_fun_mulu 
+                | r_fun_div | r_fun_divu =>
+                  current_state <= state_fetch;
+                when others =>
+                  current_state <= state_alu_wb;
+              end case;
+
+            when state_alu_imm =>
+              current_state <= state_alu_imm_wb;
+
+            when others =>
+              current_state <= state_fetch;
+
+          end case;
+      end case;
+    end if;
+  end process;
+
+  state <= current_state;
+end behave;
+
---
+++ test/fsm_test.rb
@@ -0,0 +1,42 @@
+FsmTest.dsl do
+  assign :opcode, :funct, :alu_bool_result, :reset, :state
+  clock :clk
+  require_package "work.const_state", "work.const_opcode"
+
+  testcases = {
+    i_op_lw:   ["state_memadr", "state_mem_read", "state_mem_wb"],
+    i_op_sw:   ["state_memadr", "state_mem_write"],
+    i_op_beq:  ["state_branch"],
+    i_op_addi: ["state_alu_imm", "state_alu_imm_wb"],
+    j_op_j:    ["state_jmp"],
+    j_op_jal:  ["state_jal"]
+  }
+
+  testcases.each do |k, v|
+    step 0, 0, 0, 1, "state_fetch"
+    step 0, 0, 0, 0, "state_decode"
+    step k.to_s, 0, 0, 0, v[0] 
+    step k.to_s, 0, 0, 0, v[1] if v.size > 1 
+    step k.to_s, 0, 0, 0, v[2] if v.size > 2 
+    step k.to_s, 0, 0, 0, "state_fetch"
+  end
+
+  r_fun_tests = {
+    r_fun_add:  ["state_alu", "state_alu_wb"],
+    r_fun_mul:  ["state_alu"],
+    r_fun_jr:   ["state_jmpr"],
+    r_fun_jalr: ["state_jalr"]
+  }
+
+  r_fun_tests.each do |k, v|
+    step 0, 0, 0, 1, "state_fetch"
+    step 0, 0, 0, 0, "state_decode"
+    step "i_op_r_group", k.to_s, 0, 0, v[0] 
+    step "i_op_r_group", k.to_s, 0, 0, v[1] if v.size > 1 
+    step "i_op_r_group", k.to_s, 0, 0, v[2] if v.size > 2 
+    step "i_op_r_group", k.to_s, 0, 0, "state_fetch"
+  end
+
+
+end
+
