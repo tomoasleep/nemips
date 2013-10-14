b495eb570e08b6337ee6b33a43aa75f1938be0ab
Tomoya Chiba <tomo.asleep@gmail.com>
2013-10-14 20:39:04 +0900

    Update VHDL const package generator

--- utils/opcode_gen.rb
+++ utils/opcode_gen.rb
@@ -1,10 +1,56 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
 require "erb"
 require "yaml"
+require 'trollop'
 
-@opcodes = Hash.new
-yaml = (YAML.load(File.read File.expand_path("../data/opcode.yml", __FILE__)))
-yaml.each { |k, v| v.each { |name, num| @opcodes["#{k}_#{name}"] = num }}
-erb = ERB.new(File.read File.expand_path("../templetes/opcode.vhd.erb", __FILE__))
-print erb.result(binding)
+class OpcodeGenerator
+  attr_reader :content
 
+  def initialize(yaml_path, templete_path)
+    yaml = YAML.load(File.read yaml_path)
+    erb = ERB.new(File.read templete_path)
+    @package_name = "const_" + File.basename(yaml_path, ".*")
+
+    @descs = yaml.map do |key, value|
+      ConstDescription.new(value, key)
+    end
+    @content =  erb.result(binding)
+  end
+
+  def self.run(yaml_path, templete_path)
+    gen = self.new(yaml_path, templete_path)
+    print gen.content
+    gen
+  end
+end
+
+class ConstDescription
+  attr_reader :opcodes, :num_of_digits, :key_length
+  def initialize(hsh, key)
+    @opcodes = Hash.new
+    hsh.each do |name, num| 
+      @opcodes["#{key}_#{name}"] = num 
+    end
+
+    @num_of_digits = hsh.max {|a, b| a[1] <=> b[1] }[1].to_s(2).length
+    @key_length = hsh.max {|a, b| a[0].length <=> b[0].length}[0].length +
+      key.length + 3
+
+  end
+end
+
+default_templete_path = File.expand_path("../templetes/opcode.vhd.erb", __FILE__)
+
+unless ARGV.size >= 1
+  $stderr.puts "Usage: opcode_gen YAML_PATH"
+  exit 1
+end
+
+opts = Trollop::options do
+  opt :templete, "Templete file.", type: :string, default: default_templete_path
+end
+
+
+OpcodeGenerator.run(ARGV[0], opts[:templete])
 
