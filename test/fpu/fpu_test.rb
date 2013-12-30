75595fd74658a9f74db29fe3aca72db685e6e839
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-30 16:37:59 +0900

    Add lwf test

--- test/fpu/fpu_test.rb
+++ test/fpu/fpu_test.rb
@@ -93,3 +93,31 @@ F1:
     step read_length: "io_length_byte", read_ready: 0
   end
 end
+
+VhdlTestScript.scenario "../tb/nemips_tbq.vhd", :lwf do
+  asm = %q{
+.data
+F1:
+.float 1.0
+.text
+  main:
+    la r2, F1
+    lwf f2, 0(r2)
+    fmvi r4, f2
+    ow r4
+    halt
+  }
+  inst_path = InstRam.from_asm(asm).path
+
+  dependencies inst_path, *path_dependencies
+
+  generics io_wait: 1
+  clock :clk
+
+  context "can la and lwf" do
+    wait_step 400
+    step read_length: "io_length_word", read_data: 1.0.to_binary, read_ready: 1
+    step read_length: "io_length_byte", read_ready: 0
+  end
+end
+
