404f8a2f1ed6d4efc9a3c4ea39a8fbd98d018307
Tomoya Chiba <tomo.asleep@gmail.com>
2014-01-05 17:08:51 +0900

    Add fdiv test

--- test/fpu/finv_test.rb
+++ test/fpu/finv_test.rb
@@ -61,18 +61,78 @@ F1:
   end
 end
 
+# VhdlTestScript.scenario "../tb/nemips_tbq.vhd", :fdiv do
+#   asm = %q{
+# .data
+# F1:
+# .float 2.0 
+# .text
+#   main:
+#     la  r10, F1
+#     lwf f10, 0(r10)
+#     fdiv f4, f10, f10
+#     fmvi r4, f4
+#     ow r4
+#     break
+#     halt
+#   }
+#   inst_path = InstRam.from_asm(asm).path
+# 
+#   dependencies inst_path, *dep_pathes
+# 
+#   generics io_wait: 1
+#   clock :clk
+# 
+#   context "2.0 / 2.0 = 1.0" do
+#     wait_step 400
+#     step read_length: "io_length_word", read_data: 1.0.to_binary, read_ready: 1
+#     step read_length: "io_length_byte", read_ready: 0
+#   end
+# end
+# 
+# VhdlTestScript.scenario "../tb/nemips_tbq.vhd", :fdiv do
+#   asm = %q{
+# .data
+# F1:
+# .float 4.0 
+# .float 2.0 
+# .text
+#   main:
+#     la  r10, F1
+#     lwf f10, 0(r10)
+#     lwf f11, 1(r10)
+#     fdiv f4, f10, f11
+#     owf f4
+#     break
+#     halt
+#   }
+#   inst_path = InstRam.from_asm(asm).path
+# 
+#   dependencies inst_path, *dep_pathes
+# 
+#   generics io_wait: 1
+#   clock :clk
+# 
+#   context "4.0 / 2.0 = 2.0" do
+#     wait_step 400
+#     step read_length: "io_length_word", read_data: 2.0.to_binary, read_ready: 1
+#     step read_length: "io_length_byte", read_ready: 0
+#   end
+# end
+
 VhdlTestScript.scenario "../tb/nemips_tbq.vhd", :fdiv do
   asm = %q{
 .data
 F1:
+.float 3.0 
 .float 2.0 
 .text
   main:
     la  r10, F1
     lwf f10, 0(r10)
-    fdiv f4, f10, f10
-    fmvi r4, f4
-    ow r4
+    lwf f11, 1(r10)
+    fdiv f4, f10, f11
+    owf f4
     break
     halt
   }
@@ -83,9 +143,9 @@ F1:
   generics io_wait: 1
   clock :clk
 
-  context "2.0 / 2.0 = 1.0" do
+  context "3.0 / 2.0 = 1.5" do
     wait_step 400
-    step read_length: "io_length_word", read_data: 1.0.to_binary, read_ready: 1
+    step read_length: "io_length_word", read_data: 0x3fbfffff, read_ready: 1
     step read_length: "io_length_byte", read_ready: 0
   end
 end
