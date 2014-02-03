51556f728a6c9140a0632be64c94cae46032df57
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-04 01:49:22 +0900

    Add funct_of_order function

--- src/utils/order_utils.vhd
+++ src/utils/order_utils.vhd
@@ -10,6 +10,10 @@ package order_utils is
   function opcode_of_order (
     order: in order_type
   ) return opcode_type;
+
+  function funct_of_order (
+    order: in order_type
+  ) return funct_type;
 end order_utils;
 
 package body order_utils is
@@ -20,4 +24,12 @@ package body order_utils is
   begin
     opcode := order(31 downto 26);
   end opcode_of_order;
+
+  function funct_of_order (
+    order: in order_type
+  ) return funct_type is
+    variable funct : funct_type;
+  begin
+    funct := order(5 downto 0);
+  end funct_of_order;
 end order_utils;
