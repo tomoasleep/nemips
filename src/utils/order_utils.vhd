dd447c7efaa14a274f37bc212ac7230be59b5b88
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-04 02:02:56 +0900

    Add (rs|rt|rd)_of_order

--- src/utils/order_utils.vhd
+++ src/utils/order_utils.vhd
@@ -14,6 +14,18 @@ package order_utils is
   function funct_of_order (
     order: in order_type
   ) return funct_type;
+
+  function rs_of_order (
+    order: in order_type
+  ) return register_addr_type;
+
+  function rt_of_order (
+    order: in order_type
+  ) return register_addr_type;
+
+  function rd_of_order (
+    order: in order_type
+  ) return register_addr_type;
 end order_utils;
 
 package body order_utils is
@@ -23,6 +35,7 @@ package body order_utils is
     variable opcode : opcode_type;
   begin
     opcode := order(31 downto 26);
+    return opcode;
   end opcode_of_order;
 
   function funct_of_order (
@@ -31,5 +44,33 @@ package body order_utils is
     variable funct : funct_type;
   begin
     funct := order(5 downto 0);
+    return funct;
   end funct_of_order;
+
+  function rs_of_order (
+    order: in order_type
+  ) return register_addr_type is
+    variable rs : register_addr_type;
+  begin
+    rs := order(25 downto 21);
+    return rs;
+  end rs_of_order;
+
+  function rt_of_order (
+    order: in order_type
+  ) return register_addr_type is
+    variable rt : register_addr_type;
+  begin
+    rt := order(20 downto 16);
+    return rt;
+  end rt_of_order;
+
+  function rd_of_order (
+    order: in order_type
+  ) return register_addr_type is
+    variable rd : register_addr_type;
+  begin
+    rd := order(15 downto 11);
+    return rd;
+  end rd_of_order;
 end order_utils;
