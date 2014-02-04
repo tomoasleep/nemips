92323d57c035643484cbbffbcf8a9684e633e8fa
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-04 15:29:39 +0900

    Enable View use partial views

--- utils/helper/view.rb
+++ utils/helper/view.rb
@@ -11,15 +11,40 @@ module Nemips::Utils
         @template_path = "utils/templates/#{template_name}"
       end
 
-      def initialize(template_path, view_helper)
+      def initialize(template_path, view_helper, &partial_renderer)
         @template_path = template_path
         @view_helper = view_helper
+        @original_partial_renderer = partial_renderer
+        @partial_renderer = methodnize(partial_renderer)
+      end
+
+      def methodnize(block)
+        case block
+        when Proc, Method
+          block
+        when Symbol
+          @view_helper.method(block)
+        else
+          self.method(:render_partial)
+        end
       end
 
       def run
         return @content if @content
         erb = ERB.new(File.read(@template_path), nil, '-')
-        @content = @view_helper.instance_eval { erb.result(binding) }
+        @content = erb.result(context(&@partial_renderer))
+      end
+
+      private
+      def context
+        @view_helper.instance_eval { binding }
+      end
+
+      def render_partial(path, options = {})
+        options = { as: @view_helper }.merge(options)
+        self.class.new(
+          self.class.template_path(path), options[:as], &@original_partial_renderer
+        ).run
       end
     end
   end
