95a5811eaa2e08ce5e791c2725f62c47cef3a2d7
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-27 22:15:08 +0900

    Let BootloaderMaker shift jump addrs of bootloader

--- test/copper/nemips_helper.rb
+++ test/copper/nemips_helper.rb
@@ -1,4 +1,5 @@
 require_relative '../asm_helper'
+require_relative '../nemips/bootloader_helper'
 
 class NemipsTestRunner
   class << self
@@ -22,6 +23,17 @@ class NemipsTestRunner
     end
   end
 
+  def bootloader
+    code = File.read(pfr('test/asm/bootloader.s'))
+    @before = proc do
+      path = Bootloader.from_asm(code).make_vhdl(@dir)
+      unless @added
+        Copper.load_vhdl(path)
+        @added = true
+      end
+    end
+  end
+
   def binary(bin)
     path = InstRam.new(bin).make_vhdl(@dir)
     unless @added
@@ -37,5 +49,72 @@ class NemipsTestRunner
     block = set_clock unless block
     Copper::Scenario::Circuit.configure(:nemips_tb, { before: @before }, &block)
   end
+end
+
+module InstructionSend
+  def write_insts_from_asm(inst_str)
+    write_insts InstRam.from_asm(inst_str).instructions
+  end
+
+  def write_insts(insts)
+    dut = @dut
+    context 'write instruction' do
+      step {
+        assign dut.write_length => 'io_length_none'
+      }
+      [*insts, -1].each do |i|
+        step {
+          assign dut.write_length => 'io_length_word'
+          assign dut.write_data => i
+        }
+        step {
+          assign dut.write_length => 'io_length_none'
+          assign dut.write_data => 0
+        }
+        wait_for 100
+      end
+      wait_for 60
+    end
+  end
+
+  def check(insts)
+    dut = @dut
+    context 'read instruction' do
+      step {
+        assign dut.read_length => 'io_length_none'
+      }
+      [*insts, -1].each do |i|
+        step {
+          assign dut.read_length => 'io_length_word'
+          assert dut.read_data_past => i
+        }
+      end
+      step {
+        assign dut.read_length => 'io_length_none'
+      }
+    end
+  end
+
+  def write_data(insts)
+    dut = @dut
+    context 'write instruction' do
+      step {
+        assign dut.write_length => 'io_length_none'
+      }
+      [*insts, -1].each do |i|
+        step {
+          assign dut.write_length => 'io_length_word'
+          assign dut.write_data => i
+        }
+        step {
+          assign dut.write_length => 'io_length_none'
+          assign dut.write_data => 0
+        }
+        wait_for 10
+      end
+      wait_for 120 * insts.length
+    end
+  end
+
 end
 
--- utils/bootloader_maker.rb
+++ utils/bootloader_maker.rb
@@ -1,4 +1,5 @@
 require_relative './inst_ram_maker.rb'
+require_relative './inst_analyzer'
 
 class Bootloader < InstRam
   def inst_template_path
@@ -15,5 +16,18 @@ class Bootloader < InstRam
 
   def jump_to_bootloader
     "000010#{bootloader_start.to_s(2).rjust(26, '0')}"
+  end
+
+  def shift_jump(insts)
+    InstAnalyzer.from_array(insts).instobjs.map do |inst|
+      case inst
+      when InstAnalyzer::JopInstruction
+        instbin = inst.inst
+        instbin.addr += bootloader_start
+        instbin.bin
+      else
+        inst.inst.bin
+      end
+    end
   end
 end
