c8746597e7102883a5146b32c6a413194ebb143c
Tomoya Chiba <tomo.asleep@gmail.com>
2014-03-04 12:13:07 +0900

    Fix code style of cache

--- src/cache.vhd
+++ src/cache.vhd
@@ -24,15 +24,14 @@ entity cache is
 end cache;
 
 architecture behave of cache is
-  type cache_cell is record
-    tag : std_logic_vector(9 downto 0);
-    data : word_data_type;
-  end record;
 
-  type cache_array is array(0 to 2 ** 10 - 1) of cache_cell;
-  signal cache_ram : cache_array;
+  type cache_data_array is array(0 to 2 ** 10 - 1) of word_data_type;
+  type cache_tag_array is array(0 to 2 ** 10 - 1) of std_logic_vector(9 downto 0);
+  signal cache_data_ram : cache_data_array;
+  signal cache_tag_ram : cache_tag_array;
 
-  signal read_cell : cache_cell;
+  signal read_cell_data : word_data_type;
+  signal read_cell_tag : std_logic_vector(9 downto 0);
 
   alias read_tag : std_logic_vector(9 downto 0) is read_address(19 downto 10);
   alias read_idx : std_logic_vector(9 downto 0) is read_address(9 downto 0);
@@ -40,14 +39,15 @@ architecture behave of cache is
   alias write_tag : std_logic_vector(9 downto 0) is write_address(19 downto 10);
   alias write_idx : std_logic_vector(9 downto 0) is write_address(9 downto 0);
 begin
-  read_cell <= cache_ram(to_integer(unsigned(read_idx)));
-  read_data <= read_cell.data;
-  hit <= read_cell.tag = read_tag;
+  read_cell_data <= cache_data_ram(to_integer(unsigned(read_idx)));
+  read_cell_tag <= cache_tag_ram(to_integer(unsigned(read_idx)));
+  read_data <= read_cell_data;
+  hit <= read_cell_tag = read_tag;
 
   process(clk) begin
     if rising_edge(clk) and write_enable then
-      cache_ram(to_integer(unsigned(write_idx))).data <= write_data;
-      cache_ram(to_integer(unsigned(write_idx))).tag <= write_tag;
+      cache_data_ram(to_integer(unsigned(write_idx))) <= write_data;
+      cache_tag_ram(to_integer(unsigned(write_idx))) <= write_tag;
     end if;
   end process;
 end behave;
